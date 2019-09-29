bdbe1702ddb4ab14fb08149254d8f8595f5bfa47
Harshit Maurya <hmaurya999@gmail.com>
2019-09-29 07:26:22 +0530

    Add Election contracts for candidates and voters

---
+++ contracts/Election.sol
@@ -0,0 +1,37 @@
+pragma solidity 0.5.8;
+
+contract Election {
+    // Model a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Read/write candidates
+    mapping(uint => Candidate) public candidates;
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+
+    // Store candidates count
+    uint public candidatesCount;
+
+    // Constructor
+    constructor() public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        require(!voters[msg.sender]);
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
+    }
+}
